7d774156fa8291600ed1a5a3fdf02740b8f7a761
Raquel Niehues <niehues.raquel@gmail.com>
2025-11-11 08:15:07 -0300

    Inserindo os styles CSS

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,12 @@
 import styles from "./Footer.module.css";
 
 const Footer = () => {
-  return;
-  <Footer>
-    <h3>Escreve alguma coisa aí</h3>
-    <p>Blogito &copy; 2025</p>
-  </Footer>;
+  return (
+    <footer className={styles.footer}>
+      <h3>Escreva o que vier na telha aí!</h3>
+      <p>Blogito &copy; 2025</p>
+    </footer>
+  );
 };
 
 export default Footer;

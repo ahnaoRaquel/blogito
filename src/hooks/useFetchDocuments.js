f5f777b75d3abc92ce61c5cb4dfa126b67837e13
Raquel Niehues <niehues.raquel@gmail.com>
2025-11-13 16:01:17 -0300

    Implementando a tela de post individual.

--- src/hooks/useFetchDocuments.js
+++ src/hooks/useFetchDocuments.js
@@ -57,7 +57,7 @@ export const useFetchDocuments = (docCollection, search = null, uid = null) => {
       setLoading(false);
     }
     loadData();
-  }, [docCollection, search, uid, cancelled]);
+  }, [docCollection, documents, search, uid, cancelled]);
 
   console.log(documents);
 
